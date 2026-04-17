0548900835b42d7ae8e69c7c456a1cef326f3aa4
陈权 <16424827+camellia_cpp@user.noreply.gitee.com>
2026-04-17 12:14:26 +0800

    v5

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,7 +4,7 @@ window.SURPRISE_CONFIG = {
   senderName: "烙铁",
   examName: "专升本考试",
   openingLine: "你真的已经努力很久很久了，这份坚持特别了不起。",
-  customWish: "一鸣从此始，相望青云端。祝你成功上岸，前程似锦！相信经过这么久的努力，你一定可以！",
+  customWish: "一鸣从此始，相望青云端。\n祝你成功上岸，前程似锦！相信经过这么久的努力，你一定可以！",
   endingSignature: "加油，我在终点为你鼓掌。",
   hiddenMessage: "偷偷告诉你：你不仅会过线，还会超常发挥。"
 };
